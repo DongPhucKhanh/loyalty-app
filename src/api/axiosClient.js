255509b56db6553079963c6cc8943dc4249bba7b
NguyenPhiHungHitu <bright2010gh@gmail.com>
2025-12-17 00:28:09 +0700

    update api

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://https://loyalty-backend-nftv.onrender.com/api', 
+  baseURL: 'https://loyalty-backend-nftv.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
