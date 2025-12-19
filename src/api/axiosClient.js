ec53cdc192ee08a2dd314fe946930765668a1422
NguyenPhiHungHitu <bright2010gh@gmail.com>
2025-12-19 14:29:01 +0700

    fix api url variable

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/api', // Địa chỉ Backend Spring Boot
+    baseURL: import.meta.env.VITE_API_URL, // Địa chỉ Backend Spring Boot
     headers: {
         'Content-Type': 'application/json',
     },
